a3532d9d819a86fe5fa5c19ac8c9fdc6afc5b6cf
cemilycollins <c.emily.collins@gmail.com>
2019-10-06 13:11:22 -0400

    Added GET route for form responses

---
+++ formResponses.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/kataras/iris"
+
+var formIDs = map[string]int{
+	"connect_card_itech":         0,
+	"connect_card_jdd":           0,
+	"growth_track_sign_up_itech": 0,
+	"growth_track_sign_up_jdd":   0,
+}
+
+func formResponsesGet(ctx iris.Context) {
+
+}
